d42fde177d35cab37fe1f8de11fcffd0acea4a6d
I Wayan Budiastra <wayan.budiastra26@gmail.com>
2026-02-24 09:25:29 +0800

    update fix dokument kontrol

--- app/Http/Livewire/Document/DocumentKontrol.php
+++ app/Http/Livewire/Document/DocumentKontrol.php
@@ -10,6 +10,31 @@ class DocumentKontrol extends Component
 {
     public $search;
 
+     public function edit($id)
+    {
+        return redirect()->to('/document-view/' . $id);
+    }
+
+    public function update($id)
+    {
+        return redirect()->to('/document-view/' . $id);
+    }
+
+       public function update_data($id)
+    {
+        return redirect()->to('/document-update/' . $id);
+    }
+
+    public function aktifkan($id)
+    {
+        $data = Dokument::find($id);
+        $data->update(['aktif' => 'Y']);
+    }
+    public function nonaktifkan($id)
+    {
+        $data = Dokument::find($id);
+        $data->update(['aktif' => 'N']);
+    }
     public function render()
     {
        $data = Dokument::whereBetween('masa_berlaku', [
